2e391b7217f18426c6bd1385954648d12f459626
Ethan Chen <ethan@Ethans-MacBook-Pro.local>
2023-06-17 00:21:27 -0700

    accounts for first 6 rounds of 13 prelims

--- bot.py
+++ bot.py
@@ -4,21 +4,16 @@ import numpy as np
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 
-url = "https://www.tabroom.com/index/tourn/results/event_results.mhtml?tourn_id=26661&result_id=276436"
+# TODO: implement searching for url with text fields later
+id = "276618" # can change
+url = f"https://www.tabroom.com/index/tourn/results/event_results.mhtml?tourn_id=26661&result_id={id}"
 
 # cmp stands for competitor
-# with open(url, 'r') as f: # r = read mode
-#     doc = BeautifulSoup(f, "html.parser")
-# 
-# print(doc)
 
 response = requests.get(url)
-# print(response.content)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 all_elements = soup.find_all('td', class_='rightalign smallish')
-# print(soup.get_text()[:100])
-# print(len(all_elements))
 ptn = r'\n.+(\d)\s(\d)\n'
 
 def get_nums(s):
@@ -35,7 +30,7 @@ counter = 0 # for each competitor
 big_list = np.array([])
 cmp_rankings = np.array([])
 for idx, element in enumerate(all_elements):
-    if (idx + 1) % 8 == 0 or (idx + 2) % 8 == 0:
+    if idx % 13 in list(range(6, 13)):
         continue
     # print(element.text)
     # print(idx, element)
@@ -53,15 +48,19 @@ for idx, element in enumerate(all_elements):
 
 
 big_list = np.sort(big_list)
-less_35 = np.count_nonzero(big_list <= 35)
-less_45 = np.count_nonzero(big_list <= 45)
-                           
+less_36 = np.count_nonzero(big_list < 36)
+leq_44 = np.count_nonzero(big_list <= 44)
+counts = np.unique(big_list, return_counts=True)
+
 print(big_list)
 print(f"number of competitors is {len(big_list)}")
 print(f"60th highest ranking is {big_list[59]}")
-print(f"number of competitors <= 35 is {less_35}")
-print(f"number of competitors > 35 and <= 45 is {less_45 - less_35}")
+print(f"number of competitors < 36 is {less_36}")
+print(f"number of competitors <= 44 is {leq_44}")
+print(f"number of competitors >= 36 and <= 44 is {leq_44 - less_36}")
+for i, j in zip(counts[0], counts[1]):
+    print(int(i), j)
 plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':200})
 plt.hist(big_list, bins=20)
-plt.gca().set(title='NSDA OO Prelims Ranking Distribution', ylabel='Count', xlabel='Sum of rankings');
-plt.show()
+plt.gca().set(title='NSDA OO Prelims Ranking Distribution', ylabel='Count', xlabel='Sum of rankings')
+#plt.show()
